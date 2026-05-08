5662dcf90fa3e8997fa7dcdcf2b0518cbf4c7b84
cirodamicooo04 <cirodamico04@gmail.com>
2026-05-08 19:28:02 +0200

    add check for docs directory

--- rag_backend/app/main.py
+++ rag_backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 
 from app.api.endpoints import router
+from app.core.config import UPLOAD_DIR
 from app.db.database import engine
 from app.db import models
 
@@ -8,6 +9,9 @@ import app.core.ml_models
 
 #Creo tabelle se non esistono
 models.Base.metadata.create_all(bind=engine)
+
+#Creo la cartella docs per i file caricati
+UPLOAD_DIR.mkdir(parents=True, exist_ok=True)
 
 app = FastAPI(title="RAG")
 
